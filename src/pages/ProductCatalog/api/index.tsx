ff9c02fb8f9a7d86db5a11c66aa2117291fd4407
Kofanov Ruslan <ruslan.gunder@gmail.com>
2023-04-17 17:21:42 +0300

    eslint edit;

--- src/pages/ProductCatalog/api/index.tsx
+++ src/pages/ProductCatalog/api/index.tsx
@@ -3,7 +3,7 @@ import { AllFilters } from '../index'
 
 const API = process.env.REACT_APP_API
 
-export const getProducts: (productsConfig: AllFilters) => Promise<AxiosResponse> = (data) => {
+export const getProducts = (data: AllFilters): Promise<AxiosResponse> => {
   return axios.post(`${API}/products`, {
     ...data,
   })
