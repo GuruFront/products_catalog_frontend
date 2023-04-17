732768b2318469313b47a9ce9501a89b834ae442
Kofanov Ruslan <ruslan.gunder@gmail.com>
2023-04-17 23:20:28 +0300

    import testing-library;

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import Pages from './pages'
 import CssBaseline from '@mui/material/CssBaseline'
+import '@testing-library/jest-dom'
 
 const root = createRoot(document.getElementById('root'))
 
