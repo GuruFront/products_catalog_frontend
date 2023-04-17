c33f1607bd5aa7460a96ff178fc56a36071e0e3e
Ruslan <ruslan.gunder@gmail.com>
2023-04-17 15:04:15 +0300

    reset css added; pre-commit added;

--- cypress/e2e/product_categories/getProductCategories.cy.js
+++ cypress/e2e/product_categories/getProductCategories.cy.js
@@ -2,7 +2,7 @@ describe('example to-do app', () => {
   beforeEach(() => {
     cy.visit('/')
   })
-
+  //TODO: add any useful test here instead
   it('displays 7 categories', () => {
     cy.get('[data-component="categories"] label').should('have.length', 7)
   })
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot} from 'react-dom/client'
 import Pages from './pages'
-import './index.scss'
+import CssBaseline from "@mui/material/CssBaseline";
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  <React.StrictMode>
-    <Pages />
-  </React.StrictMode>,
+    <React.StrictMode>
+        <CssBaseline/>
+        <Pages/>
+    </React.StrictMode>,
 )
