0746f3a5e017b4116a976d8f64a6e67ce82ea2ec
Kofanov Ruslan <ruslan.gunder@gmail.com>
2023-04-17 17:10:58 +0300

    test commit;

--- src/pages/ProductCatalog/index.tsx
+++ src/pages/ProductCatalog/index.tsx
@@ -96,7 +96,7 @@ const ProductCatalog = () => {
         )}
         <Grid container spacing={2}>
           <Grid item xs={12} md={3}>
-            {/* <ProductFilters onChange={onFilterChanged} /> */}
+            no filters
           </Grid>
           <Grid item xs={12} md={9}>
             {areProductLoading ? (
